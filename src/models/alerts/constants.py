106116ac427ddc9a2edf46c8d9272cfb5e5fea26
Pietro Mazzocco <mazzopie92@yahoo.it>
2018-02-27 18:55:12 +0100

    Update constants.py
    Modified constants values

--- src/models/alerts/constants.py
+++ src/models/alerts/constants.py
@@ -1,7 +1,7 @@
 
 
-URL = "https://api.mailgun.net/v3/sandbox55a2b6c03bd247cea4944c99c9aae1c3.mailgun.org"
-API_KEY = "key-3a4c7326a57088560f0753fbf4c50c4b"
-FROM = "postmaster@sandbox55a2b6c03bd247cea4944c99c9aae1c3.mailgun.org"
+URL = "###"
+API_KEY = "###"
+FROM = "###"
 ALERT_TIMEOUT = 10
 COLLECTION = 'alerts'
